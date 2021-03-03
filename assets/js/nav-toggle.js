75acee675dfd7cd8553b81982329d7b56ad1893c
Rathod <rathodriteshrock@gmail.com>
2021-03-03 23:09:36 +0530

    add AOS js library for scrolling animation

--- assets/js/nav-toggle.js
+++ assets/js/nav-toggle.js
@@ -1,6 +1,15 @@
 
-        var toggle=1;
-     
+        
+        if(screen.width>1024)
+        {
+            var toggle=1;
+            console.log(screen.width);
+        }   
+        else{
+            var toggle=0;
+            document.getElementById('navbar-item-toggle').style.display="none";
+        }
+
         function nav_toggle(){
             if(toggle==0)
             {
